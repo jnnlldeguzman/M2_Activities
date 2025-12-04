cd2e8deef9ae19adb21c1e8d43f94d6483e69363
Jannella Rose T. De Guzman <jrtdeguzman@headoffice.corp.bpi.com.ph>
2025-12-04 18:40:04 +0800

    Seatwork1 house created

---
+++ S1House.java
@@ -0,0 +1,27 @@
+package Individual;
+
+public class S1House {
+
+	
+	String location, color;
+	float size;
+	
+	
+	void displaylocation(){
+		
+		System.out.println("House location: " + location);
+	}
+
+
+	void displaycolor(){
+		
+		System.out.println("House color: " + color);
+	}
+	
+//	void displaysize(){
+//		
+//		System.out.println("House size: " + size);
+//	}
+
+
+}
