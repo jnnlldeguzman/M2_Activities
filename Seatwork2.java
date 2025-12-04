be8c94103863bacd64ffe8eaf042d5e5ab3efcad
Jannella Rose T. De Guzman <jrtdeguzman@headoffice.corp.bpi.com.ph>
2025-12-04 18:43:42 +0800

    Seatwork2 part1

---
+++ Seatwork2.java
@@ -0,0 +1,20 @@
+package Individual;
+
+//import java.util.Scanner;
+
+public class Seatwork2 {
+
+	public static void main(String[] args) {
+		
+	
+		S2Whiteboard wb = new S2Whiteboard();
+		
+		int sum = wb.addNumbers (5,3);
+	
+        System.out.println("The total is: " + String.valueOf(sum));  
+		
+
+		
+	}
+
+}
