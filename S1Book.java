3fd245e0c65fcfc02aa3cbf29e95a17f8e8eed56
Jannella Rose T. De Guzman <jrtdeguzman@headoffice.corp.bpi.com.ph>
2025-12-04 18:39:01 +0800

    Seatwork1 book created

---
+++ S1Book.java
@@ -0,0 +1,15 @@
+package Individual;
+
+public class S1Book {
+
+
+	String title, author;
+	
+	
+	void display(){
+		
+		System.out.println("Book title: " + title);
+//		System.out.println("Book author: " + author);
+	}
+
+}
