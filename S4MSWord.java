88220077fb2f5eaaab6ee159c4807e319e13962e
Jannella Rose T. De Guzman <jrtdeguzman@headoffice.corp.bpi.com.ph>
2025-12-05 18:29:57 +0800

    M2-A4 part4

---
+++ S4MSWord.java
@@ -0,0 +1,15 @@
+package Individual;
+
+public class S4MSWord implements S4Executable{
+	
+		public void run() {
+			System.out.println("Opening MS Word...");
+		}
+
+		
+		public void stop() {
+			System.out.println("Stopping MS Word...");
+		}
+
+	
+}
