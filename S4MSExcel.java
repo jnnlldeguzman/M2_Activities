15eb47fdb0ea5340beae89365a30dbf8a27165ea
Jannella Rose T. De Guzman <jrtdeguzman@headoffice.corp.bpi.com.ph>
2025-12-05 18:28:23 +0800

    M2-A4 part3

---
+++ S4MSExcel.java
@@ -0,0 +1,14 @@
+package Individual;
+
+public class S4MSExcel implements S4Executable {
+	
+	public void run() {
+		System.out.println("Opening MS Excel...");
+	}
+
+	
+	public void stop() {
+		System.out.println("Stopping MS Excel...");
+	}
+
+}
